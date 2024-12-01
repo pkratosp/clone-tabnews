7c7b99dc6e2c32971c59d1b1ab4dea56d8bc4112
Pedro <70226968+pkratosp@users.noreply.github.com>
2024-12-01 02:12:06 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
 export default function Home() {
-    return (
-        <h1>Aqui vai uma frase motivacional, não desista :)</h1>
-    )
+  return <h1>Aqui vai uma frase motivacional, não desista :)</h1>;
 }
