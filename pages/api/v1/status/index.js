e12fd1d82bfb727c1de16a9dfc684e863c695d99
Pedro <70226968+pkratosp@users.noreply.github.com>
2025-01-05 02:04:52 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,37 @@
 import database from "infra/database.js";
-
+/**TODO: fazer sozinho
+ * 1- versao do postgres
+ * 2- conexoes maximas
+ * 3- conexoes usadas
+ */
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databseConnectionUsageResult = await database.query({
+    text: "SELECT COUNT(*)::int as total_connection FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const databseConnectionUsageValue =
+    databseConnectionUsageResult.rows[0].total_connection;
+
+  const updatedAt = new Date().toISOString();
+
   response.status(200).json({
-    ok: true,
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databseConnectionUsageValue,
+      },
+    },
   });
 }
 
